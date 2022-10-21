9935387a47017b013e78a4b91fd0bbcc0e94ddee
Danilo <danilo.volles@outlook.com>
2022-10-21 19:50:54 -0300

    Initial commit. Inclusion of project classes. Inclusion of Questao1 solution.

---
+++ src/Questao1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Questao1 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá! Vou te mostrar a tabuada de um número inteiro que você digitar!");
+        System.out.print("Por favor, digite um número: ");
+        int n = input.nextInt();
+
+        System.out.println("A tabuada do número " + n + " é: ");
+
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+}
